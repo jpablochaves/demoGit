696616ad7dc80f60e02dd11d0df4c8bfe1382faf
José Pablo Chaves <jchaves@ice.go.cr>
2021-07-07 16:37:20 -0600

    Se incluye una operación de Login y su funcion de JS

--- funciones.js
+++ funciones.js
@@ -1,1 +1,27 @@
-console.log("Ejemplo de GIT");
+function login_user() {
+	var username = document.getElementById("username").value; 
+  	var passwd = document.getElementById("password").value;
+	if( username == "prueba" && passwd == "prueba"){
+		window.location = "index.html?red=1&usr="+username;		
+	}else{		
+		window.location = "index.html?red=0&usr="+username;
+	}  
+}
+
+
+function get_redirect(){
+	const queryString = window.location.search;
+	const parameters = new URLSearchParams(queryString);
+	redirect_value = parameters.get('red');
+	user_value = parameters.get('usr');
+	if (redirect_value != null && user_value != null){
+		if(redirect_value == 0){
+			document.getElementById("msj_error").innerHTML = "El usuario "+user_value+" no se encuentra autorizado";		
+		}else{
+		/*	document.getElementById("msj_error").innerHTML = ""; */
+			window.location = 'welcome.html?user='+user_value;
+		}
+	}else{
+		document.getElementById("msj_error").innerHTML = "";
+	}
+}
